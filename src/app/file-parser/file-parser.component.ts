acd87770ec129eff299ff2d7600b92ec12bbf974
webstacker-io <webstacker.io@gmail.com>
2021-08-16 19:21:29 +0530

    Parsing scripts from package json files

--- src/app/file-parser/file-parser.component.ts
+++ src/app/file-parser/file-parser.component.ts
@@ -8,7 +8,9 @@ import { ElectronAdapterService } from '../services/electron-adapter.service';
 })
 export class FileParserComponent implements OnInit {
   browseMode = true;
-  
+  path: any;
+  _scriptOpitons: string [] = [];
+  selectMode = false;
 
   constructor(private electronAdapterService: ElectronAdapterService) { }
 
@@ -21,45 +23,19 @@ export class FileParserComponent implements OnInit {
       properties: ['openDirectory']
     })
     let _filePath = result.filePaths;
-    console.log(result.filePaths);
     if(result && result.canceled === false){
-      this.browseMode = false;
-      if(_filePath.length > 1){
-        
-      }else if(_filePath.length === 1){
-        if(this._checkPackageFile(_filePath[0]+ 'package.json')){
-          
-        }
-      }
+      this._parseScriptOptions(_filePath[0]);
     }
-    
+     
   }
 
-  private _checkPackageFile(folderPath): boolean{
-    return this.electronAdapterService.fs.existsSync(folderPath);
-  }
-
-  private _readPackageFile(folderPath){
-     this.electronAdapterService.FileSystem.recurse(folderPath, [
-      'package.json',
-      
-    ], function(filepath, relative, filename) {  
-      if (filename) {
-      // it's file
-      } else {
-      // it's folder
-      }
+  private _parseScriptOptions(folderPath){
+    this.electronAdapterService.FileSystem.recurse(folderPath, ['**/package.json'],(filepath, relative, filename) => { 
+      if(!filepath.includes('node_modules') && filepath !== undefined){
+        this._scriptOpitons.push(JSON.parse(this.electronAdapterService.fs.readFileSync(filepath).toString()));
+      }      
     });
-  }
-
-  private _getChildDirectories(folderPath): Array<string>{
-    return this.electronAdapterService.fs.readdirSync(folderPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-  }
-
-  private _identifyMicrofrontend(){
-
+    this.selectMode = true;
   }
 
 }
